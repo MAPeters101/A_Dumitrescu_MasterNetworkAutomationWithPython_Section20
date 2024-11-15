2f6158ec045d77b45708c9a0413d7e2456d8bc73
Mark Peters <mapeters101@earthlink.net>
2024-11-15 17:57:44 -0500

    Lesson183:  Work on lesson.

---
+++ lesson183.py
@@ -0,0 +1,39 @@
+a = 5
+print(a)
+
+a += 2
+print(a)
+
+a -= 3
+print(a)
+
+a *= 3
+print(a)
+
+a /= 2
+print(a)
+
+a **= 2
+print(a)
+
+a += 1
+print(a)
+print()
+
+a, b = divmod(14, 6)
+print(a, b)
+
+
+print(pow(5, 9))
+print(sum([1, 2, 5, 6, 7, 7, 9]))
+print(max([1, 2, 5, 6, 7, 7, 9]))
+print(min([1, 2, 5, 6, 7, 7, 9, -6]))
+print()
+
+a = 5.666772
+print(round(a, 3))
+print(a)
+b = round(a, 1)
+print(a, b)
+
+
