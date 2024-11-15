e100d74f4957a4903037a99228ddd68e9d770c4d
Mark Peters <mapeters101@earthlink.net>
2024-11-15 17:50:57 -0500

    Lesson182:  Work on lesson.

---
+++ lesson182.py
@@ -0,0 +1,22 @@
+
+print(5 + 5)
+print(3 - 9)
+print(8 / 4)
+print(11 // 2)
+print(5 * 4)
+print(2 ** 100)
+print(8 % 5)
+print(14 % 4)
+print()
+
+print(2 + 4 * 2 ** 3)
+print((2 + 4) * 2 ** 3)
+
+print(1_000_000_000)
+print(1000000000)
+print(1000 == 1_000)
+
+
+
+
+
