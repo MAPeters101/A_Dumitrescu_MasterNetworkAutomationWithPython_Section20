52c08a6df4cde6cb1ec59b15d8f66da0e5f27c5d
Mark Peters <mapeters101@earthlink.net>
2024-11-15 16:06:11 -0500

    Lesson178:  Work on lesson.

---
+++ lesson178.py
@@ -0,0 +1,17 @@
+# This is a comment
+
+# a = 5
+
+# x = 9 # Here I create a variable called x that stores an integer(9)
+# my_str = 'A hash # within a string literal is just a hash char.'
+# a = 10
+# print('Hello Python!')
+
+
+"""
+x = 10
+print(x)
+"""
+
+
+
